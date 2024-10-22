0c8f0ea6567bc60c886ed4bb34fd531460bd2779
Sparsha <work.sparsha@gmail.com>
2024-10-22 01:26:01 -0400

    till My logic

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-let boxes = 
-
-
-
+let boxes = document.querySelectorAll('.box')
 
 function user1(){
-
+let value='x'
+boxes.forEach((box)=>{
+    if(box.textContent==''){
+        box.addEventListener('click',()=> box.textContent=value)
+        
+    }
+})
 }
 
 function user2(){
-
+let value='0'
+boxes.forEach((box)=>{
+    if(box.textContent==''){
+        box.addEventListener('click',()=> box.textContent=value)
+    }
+})
 }
 
 function bot(){
 
 }
+winningCombinations=[
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+]
+
+
+
+user1()
